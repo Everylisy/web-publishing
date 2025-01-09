c774934e4f7592f6ef1e8089f2e58470621dc29f
Everylisy <nta72986@gmail.com>
2025-01-09 16:23:26 +0900

    style: burger 버튼 클릭 시 전체화면으로 메뉴가 열리도록 구현

--- niceghostclub-com/script.js
+++ niceghostclub-com/script.js
@@ -1,8 +1,20 @@
 const sortButton = document.getElementById('sort-btn');
 const sortList = document.getElementById('sort-list-wrapper');
 
+const burgerBtn = document.querySelector('.burger-btn');
+const menuOpenBtn = document.querySelector('.menu-open-btn');
+const menuCloseBtn = document.querySelector('.menu-close-btn');
+const burgerMenu = document.querySelector('.burger-menu-wrapper');
+
 const clickSortButton = () => {
 	sortList.classList.toggle('active');
 };
 
+const clickBurgerBtn = () => {
+	menuOpenBtn.classList.toggle('inactive');
+	burgerMenu.classList.toggle('active');
+	menuCloseBtn.classList.toggle('active');
+};
+
 sortButton.addEventListener('click', clickSortButton);
+burgerBtn.addEventListener('click', clickBurgerBtn);
