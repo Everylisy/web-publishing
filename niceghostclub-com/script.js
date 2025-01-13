9e93399bc00a58bfc703a10c32d8c653dd8884cc
Everylisy <nta72986@gmail.com>
2025-01-13 22:16:48 +0900

    style: 오른쪽에서 열리는 팝업 로그인 ui 구현

--- niceghostclub-com/script.js
+++ niceghostclub-com/script.js
@@ -1,4 +1,4 @@
-const sortButton = document.getElementById('sort-btn');
+const sortBtn = document.getElementById('sort-btn');
 const sortList = document.getElementById('sort-list-wrapper');
 
 const burgerBtn = document.querySelector('.burger-btn');
@@ -6,7 +6,12 @@ const menuOpenBtn = document.querySelector('.menu-open-btn');
 const menuCloseBtn = document.querySelector('.menu-close-btn');
 const burgerMenu = document.querySelector('.burger-menu-wrapper');
 
-const clickSortButton = () => {
+const profileBtn = document.querySelector('.profile-btn');
+const popup = document.querySelector('.popup-wrapper');
+const popupBackground = document.querySelector('.popup-background');
+const popupCloseBtn = document.querySelector('.popup-close-btn');
+
+const clickSortBtn = () => {
 	sortList.classList.toggle('active');
 };
 
@@ -16,5 +21,17 @@ const clickBurgerBtn = () => {
 	menuCloseBtn.classList.toggle('active');
 };
 
-sortButton.addEventListener('click', clickSortButton);
+const openPopup = () => {
+	popup.classList.add('active');
+	popupBackground.classList.add('active');
+};
+
+const closePopup = () => {
+	popup.classList.remove('active');
+	popupBackground.classList.remove('active');
+};
+
+sortBtn.addEventListener('click', clickSortBtn);
 burgerBtn.addEventListener('click', clickBurgerBtn);
+profileBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
