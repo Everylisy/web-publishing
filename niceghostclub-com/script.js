e97a049e602dda420bd78eb3deea93c6e909a4e2
Everylisy <nta72986@gmail.com>
2025-01-14 21:29:04 +0900

    feat: 팝업에서 로그인/장바구니 각 버튼에 맞는 ui가 표시되도록 구현

--- niceghostclub-com/script.js
+++ niceghostclub-com/script.js
@@ -10,9 +10,13 @@ const menuCloseBtn = document.querySelector('.menu-close-btn');
 const burgerMenu = document.querySelector('.burger-menu-wrapper');
 
 const profileBtn = document.querySelector('.profile-btn');
+const shoppingBagBtn = document.querySelector('.shopping-bag-btn');
 const popup = document.querySelector('.popup-wrapper');
 const popupBackground = document.querySelector('.popup-background');
 const popupCloseBtn = document.querySelector('.popup-close-btn');
+
+const loginTab = document.querySelector('.login-outer-wrapper');
+const shoppingBagTab = document.querySelector('.shopping-bag-outer-wrapper');
 
 const clickSortBtn = () => {
 	sortList.classList.toggle(ACTIVE);
@@ -20,8 +24,8 @@ const clickSortBtn = () => {
 
 const clickBurgerBtn = () => {
 	menuOpenBtn.classList.toggle(INACTIVE);
-	burgerMenu.classList.toggle(ACTIVE);
 	menuCloseBtn.classList.toggle(ACTIVE);
+	burgerMenu.classList.toggle(ACTIVE);
 };
 
 const openPopup = () => {
@@ -34,7 +38,21 @@ const closePopup = () => {
 	popupBackground.classList.remove(ACTIVE);
 };
 
+const openLoginTab = () => {
+	shoppingBagTab.classList.remove(ACTIVE);
+	loginTab.classList.add(ACTIVE);
+};
+
+const openShoppingBagTab = () => {
+	loginTab.classList.remove(ACTIVE);
+	shoppingBagTab.classList.add(ACTIVE);
+};
+
 sortBtn.addEventListener('click', clickSortBtn);
 burgerBtn.addEventListener('click', clickBurgerBtn);
+
 profileBtn.addEventListener('click', openPopup);
+profileBtn.addEventListener('click', openLoginTab);
+shoppingBagBtn.addEventListener('click', openPopup);
+shoppingBagBtn.addEventListener('click', openShoppingBagTab);
 popupCloseBtn.addEventListener('click', closePopup);
