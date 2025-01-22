23af7be5d75016d92b21311bccd440a3fba3e042
Everylisy <nta72986@gmail.com>
2025-01-22 17:26:51 +0900

    feat: 쇼핑정보 수신 전체동의 기능 구현

--- niceghostclub-com/js/signUp.js
+++ niceghostclub-com/js/signUp.js
@@ -2,6 +2,12 @@ const agreeAllCheckbox = document.getElementById('agree-all-checkbox');
 const checkboxes = document.querySelectorAll('.terms-checkbox-wrapper input');
 const checkboxArr = [...checkboxes];
 
+const receiveInfoCheckbox = document.getElementById('recieve-info-checkbox');
+const smsEmailCheckboxes = document.querySelectorAll(
+	'.sms-email-option-wrapper input'
+);
+const smsEmailCheckboxArr = [...smsEmailCheckboxes];
+
 const syncCheckboxState = (arr, targetElem) => {
 	arr.map((el) => {
 		el.checked = targetElem.checked ? true : false;
@@ -12,4 +18,9 @@ const clickAgreeAll = (event) => {
 	syncCheckboxState(checkboxArr, event.target);
 };
 
+const clickRecieveInfo = (event) => {
+	syncCheckboxState(smsEmailCheckboxArr, event.target);
+};
+
 agreeAllCheckbox.addEventListener('change', clickAgreeAll);
+receiveInfoCheckbox.addEventListener('change', clickRecieveInfo);
